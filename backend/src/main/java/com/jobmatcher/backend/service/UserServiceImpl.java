9a279049e1394422f2b55bf08c3d8a6828c1cca7
builtwithlalit <boraselalit9@gmail.com>
2026-06-20 17:18:22 +0530

    feat: add User Service Impl

---
+++ backend/src/main/java/com/jobmatcher/backend/service/UserServiceImpl.java
@@ -0,0 +1,26 @@
+package com.jobmatcher.backend.service;
+
+import com.jobmatcher.backend.entity.User;
+import com.jobmatcher.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User findUserByEmail(String email) throws Exception{
+        return userRepository.findByEmail(email).orElseThrow(() -> new Exception("User not found"));
+    }
+
+    @Override
+    public User findUserByJwtToken(String jwt) throws Exception{
+        return null;
+    }
+}
