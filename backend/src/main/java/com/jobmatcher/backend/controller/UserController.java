8d082bda787e2e22ae1098e1745dabb7a6ffa98d
builtwithlalit <boraselalit9@gmail.com>
2026-06-21 17:20:18 +0530

    feat : add Job Controller

---
+++ backend/src/main/java/com/jobmatcher/backend/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.jobmatcher.backend.controller;
+
+import com.jobmatcher.backend.entity.User;
+import com.jobmatcher.backend.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        return ResponseEntity.ok(userService.saveUser(user));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) throws Exception {
+        return ResponseEntity.ok(userService.getUserById(id));
+    }
+}
