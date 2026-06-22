14a1b9c08b8d54bf556ebc0be0ac622be47b8aa5
builtwithlalit <boraselalit9@gmail.com>
2026-06-22 22:25:32 +0530

    feat: add User Service

--- backend/src/main/java/com/jobmatcher/backend/service/UserService.java
+++ backend/src/main/java/com/jobmatcher/backend/service/UserService.java
@@ -2,7 +2,15 @@ package com.jobmatcher.backend.service;
 
 import com.jobmatcher.backend.entity.User;
 
+import java.util.List;
+
 public interface UserService {
+    User saveUser(User user);
+
+    User getUserById(Long id) throws Exception;
+
+    List<User> getAllUsers();
+
     User findUserByJwtToken(String jwt) throws Exception;
 
     User findUserByEmail(String email) throws Exception;
