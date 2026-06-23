021f174da74e752eaf5cbcb3a284139ff0221272
builtwithlalit <boraselalit9@gmail.com>
2026-06-23 22:00:06 +0530

    feat: add User Service Impl

--- backend/src/main/java/com/jobmatcher/backend/service/UserServiceImpl.java
+++ backend/src/main/java/com/jobmatcher/backend/service/UserServiceImpl.java
@@ -5,6 +5,8 @@ import com.jobmatcher.backend.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
@@ -14,6 +16,21 @@ public class UserServiceImpl implements UserService {
     @Override
     public User findUserByEmail(String email) throws Exception{
         return userRepository.findByEmail(email).orElseThrow(() -> new Exception("User not found"));
+    }
+
+    @Override
+    public User saveUser(User user) {
+        return null;
+    }
+
+    @Override
+    public User getUserById(Long id) throws Exception {
+        return null;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return List.of();
     }
 
     @Override
