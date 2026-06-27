cd3d51e180740233a420d8e8bc2c8fa15c01674d
builtwithlalit <boraselalit9@gmail.com>
2026-06-27 22:57:11 +0530

    feat: add User Controller

--- backend/src/main/java/com/jobmatcher/backend/controller/UserController.java
+++ backend/src/main/java/com/jobmatcher/backend/controller/UserController.java
@@ -2,6 +2,7 @@ package com.jobmatcher.backend.controller;
 
 import com.jobmatcher.backend.entity.User;
 import com.jobmatcher.backend.service.UserService;
+import lombok.RequiredArgsC
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
