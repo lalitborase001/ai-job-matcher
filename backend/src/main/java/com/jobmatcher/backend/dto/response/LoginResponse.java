69200c7a61c40dfc5012e6642fd5f0118004f005
builtwithlalit <boraselalit9@gmail.com>
2026-07-12 02:22:52 +0530

    feat: add login request and response DTOs

---
+++ backend/src/main/java/com/jobmatcher/backend/dto/response/LoginResponse.java
@@ -0,0 +1,16 @@
+package com.jobmatcher.backend.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginResponse {
+
+    private String token;
+
+    private String message;
+
+}
