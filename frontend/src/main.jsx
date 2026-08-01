bd865c16fa7ef0278722f21fb414dad4b347fbe7
builtwithlalit <boraselalit9@gmail.com>
2026-08-01 22:04:20 +0530

    feat: configure frontend application entry point

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from "./App";
+import theme from "./theme/theme";
+import store from "./redux/store";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+
+        <Provider store={store}>
+
+            <ThemeProvider theme={theme}>
+
+                <CssBaseline />
+
+                <BrowserRouter>
+
+                    <App />
+
+                </BrowserRouter>
+
+            </ThemeProvider>
+
+        </Provider>
+
+    </React.StrictMode>
+);
