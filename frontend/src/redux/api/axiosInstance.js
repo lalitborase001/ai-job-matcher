24525073687d9cad417a2bb55feaa836488c8bfd
builtwithlalit <boraselalit9@gmail.com>
2026-08-07 11:54:08 +0530

    feat: add Axios instance configuration

---
+++ frontend/src/redux/api/axiosInstance.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import { API_BASE_URL } from '../constants/config';
+
+const axiosInstance = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      localStorage.removeItem('token');
+      window.location.href = '/login';
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
