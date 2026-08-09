1cac1168781a00c4dedd3333ff57d97a55d8f1fd
builtwithlalit <boraselalit9@gmail.com>
2026-08-10 00:44:20 +0530

    feat: configure application routing

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import ProtectedRoute from './ProtectedRoute';
 import MainLayout from '../components/layout/MainLayout';
 import Login from '../pages/Login';
+import Register from '../pages/Register';
 
 const HomePlaceholder = () => <h1>Home Page</h1>;
 const DashboardPlaceholder = () => <h1>Dashboard (Protected)</h1>;
@@ -13,6 +14,7 @@ const AppRoutes = () => {
     <Routes>
       <Route path="/" element={<HomePlaceholder />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
       
       <Route element={<ProtectedRoute />}>
         <Route element={<MainLayout />}>
