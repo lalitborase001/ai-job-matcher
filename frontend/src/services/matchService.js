96f86bb8e379a01bcc61d1dbc163efb6a3203664
builtwithlalit <boraselalit9@gmail.com>
2026-08-11 14:39:00 +0530

    feat: implement match service

--- frontend/src/services/matchService.js
+++ frontend/src/services/matchService.js
@@ -1,9 +1,7 @@
 import axiosInstance from '../api/axiosInstance';
 
 export const generateMatchAPI = async (jobId, resumeId) => {
-  const response = await axiosInstance.post('/api/match', {
-    jobId: jobId,
-    resumeId: resumeId
-  });
+  const response = await axiosInstance.post(`/api/ai/analyze/${resumeId}/${jobId}`);
+  
   return response.data;
 };
